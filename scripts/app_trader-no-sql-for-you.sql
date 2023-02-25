1d6bf690896730632577515c6693c7202e21b6db
George Correa <georgecorrea12@gmail.com>
2023-02-25 12:18:22 -0600

    push12:18pm 2-25

--- scripts/app_trader-no-sql-for-you.sql
+++ scripts/app_trader-no-sql-for-you.sql
@@ -54,46 +54,313 @@
 -- b. Develop a Top 10 List of the apps that App Trader should buy.
 -- updated 2/18/2023
 
-SELECT 
-	a.name AS apple_store_apps,
-	p.name AS play_store_apps, 
-	p.genres, a.content_rating,
- 	ROUND((a.rating + p.rating), 0)/2 AS avg_rating,
-	(((ROUND((a.rating + p.rating),0)/2) * 0.2) + 0.1) * 10 AS life_expectancy,
-	CASE
-		WHEN CAST(p.price AS MONEY) > CAST('0' AS MONEY) THEN CAST(p.price AS MONEY) * 10000
-		WHEN CAST (p.price AS MONEY)= CAST('0' AS MONEY) THEN ('10000')
-		END AS playstore_purchase_price,
-	CASE
-		WHEN a.price > 0 THEN a.price * 10000 :: MONEY
-		WHEN a.price = 0 THEN ('10000') :: MONEY
-		END AS appstore_purchase_price,
-	CASE
-		WHEN CAST(p.price AS MONEY) > a.price::MONEY THEN p.price
-		WHEN CAST(p.price AS MONEY) < a.price::MONEY THEN '19900.00'
-		WHEN CAST (p.price AS MONEY) = a.price::MONEY THEN p.price
-		END AS final_purchase_price,
-	(10000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_revenue,
-	((1000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_cost,
-	(((10000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) 
-	 	- (1000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)) :: MONEY AS expected_profit
+-- final code
+
+WITH app_trader_table AS
+(SELECT
+    a.name AS apple_store_apps,
+    p.name AS play_store_apps,
+    p.genres, a.content_rating,
+    ROUND(((a.rating + p.rating) / 2)*2/2) AS avg_rating,
+    ROUND((((a.rating + p.rating)/2)* 0.2) + 0.1,1)* 10 AS life_expectancy,
+    CASE
+    WHEN CAST(p.price AS MONEY) > CAST('0' AS MONEY) THEN CAST(p.price AS MONEY) * 10000
+    WHEN CAST(p.price AS MONEY) = CAST('0' AS MONEY) THEN CAST('10000' AS MONEY)
+    END AS playstore_purchase_price,
+    CASE
+    WHEN a.price > 0 THEN CAST(a.price * 10000 AS MONEY)
+    WHEN a.price = 0 THEN CAST('10000' AS MONEY)
+    END AS appstore_purchase_price,
+     CASE
+        WHEN a.price = 0 AND CAST(p.price AS MONEY) = CAST('0' AS MONEY) THEN CAST('10000' AS MONEY)
+        ELSE COALESCE(GREATEST(CAST(CAST(p.price AS money)*10000 AS NUMERIC),a.price * 10000),'10000')::money 
+     END AS final_purchase_price
 FROM play_store_apps AS p
 INNER JOIN app_store_apps AS a
 ON p.name = a.name
---WHERE a.price <= 1
---AND a.content_rating <> '12+'
-GROUP BY 
-	p.name,
-	a.name,
-	avg_rating,
-	p.price,
-	a.price, 
-	p.genres, 
-	a.content_rating,
-	a.rating,
-	p.rating
+GROUP BY
+    p.name,
+    a.name,
+    avg_rating,
+    p.price,
+    a.price,
+    p.genres,
+    a.content_rating,
+    a.rating,
+    p.rating
 HAVING ROUND((a.rating + p.rating) / 2, 2) >= 4
---ORDER BY avg_rating DESC
+ORDER BY avg_rating DESC)
+
+SELECT appstore_purchase_price, playstore_purchase_price, final_purchase_price, a.name, avg_rating, life_expectancy,
+	(final_purchase_price * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_revenue,
+	(1000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_cost,
+	
+	(final_purchase_price * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)
+	- (1000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12):: MONEY AS expected_profit
+FROM app_trader_table
+INNER JOIN app_store_apps AS a 
+ON app_trader_table.apple_store_apps = a.name
+INNER JOIN play_store_apps AS p 
+ON app_trader_table.play_store_apps = p.name
+GROUP BY a.name, appstore_purchase_price, playstore_purchase_price, final_purchase_price, avg_rating, life_expectancy, a.rating, p.rating
 ORDER BY expected_profit DESC
 LIMIT 10;
+
+
+
+-- vvvvvvvv old code. kept as refrence
+-- SELECT 
+-- 	a.name AS apple_store_apps,
+-- 	p.name AS play_store_apps, 
+-- 	p.genres, a.content_rating,
+--  	ROUND((a.rating + p.rating), 0)/2 AS avg_rating,
+-- 	(((ROUND((a.rating + p.rating),0)/2) * 0.2) + 0.1) * 10 AS life_expectancy,
+-- 	CASE
+-- 		WHEN CAST(p.price AS MONEY) > CAST('0' AS MONEY) THEN CAST(p.price AS MONEY) * 10000
+-- 		WHEN CAST (p.price AS MONEY)= CAST('0' AS MONEY) THEN ('10000')
+-- 		END AS playstore_purchase_price,
+-- 	CASE
+-- 		WHEN a.price > 0 THEN a.price * 10000 :: MONEY
+-- 		WHEN a.price = 0 THEN ('10000') :: MONEY
+-- 		END AS appstore_purchase_price,
+-- 	CASE
+-- 		WHEN CAST(p.price AS MONEY) > a.price::MONEY THEN p.price
+-- 		WHEN CAST(p.price AS MONEY) < a.price::MONEY THEN '19900.00'
+-- 		WHEN CAST (p.price AS MONEY) = a.price::MONEY THEN p.price
+-- 		END AS final_purchase_price,
+-- 	(10000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_revenue,
+-- 	((1000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_cost,
+-- 	(((10000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) 
+-- 	 	- (1000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)) :: MONEY AS expected_profit
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name = a.name
+-- --WHERE a.price <= 1
+-- --AND a.content_rating <> '12+'
+-- GROUP BY 
+-- 	p.name,
+-- 	a.name,
+-- 	avg_rating,
+-- 	p.price,
+-- 	a.price, 
+-- 	p.genres, 
+-- 	a.content_rating,
+-- 	a.rating,
+-- 	p.rating
+-- HAVING ROUND((a.rating + p.rating) / 2, 2) >= 4
+-- --ORDER BY avg_rating DESC
+-- ORDER BY expected_profit DESC
+-- LIMIT 10;
+
+
+-- WITH app_trader_table AS
+-- (SELECT
+--     a.name AS apple_store_apps,
+--     p.name AS play_store_apps,
+--     p.genres, a.content_rating,
+--     ROUND(((a.rating + p.rating) / 2)*2/2) AS avg_rating,
+--     ROUND((((a.rating + p.rating)/2)* 0.2) + 0.1,1)* 10 AS life_expectancy,
+--     CASE
+--     WHEN CAST(p.price AS MONEY) > CAST('0' AS MONEY) THEN CAST(p.price AS MONEY) * 10000
+--     WHEN CAST(p.price AS MONEY) = CAST('0' AS MONEY) THEN CAST('10000' AS MONEY)
+-- END AS playstore_purchase_price,
+-- CASE
+--     WHEN a.price > 0 THEN CAST(a.price * 10000 AS MONEY)
+--     WHEN a.price = 0 THEN CAST('10000' AS MONEY)
+-- END AS appstore_purchase_price,
+--     COALESCE(
+--         GREATEST(CAST(CAST(p.price AS money)*10000 AS NUMERIC),a.price * 10000),'10000')::money
+--         AS final_purchase_price
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name = a.name
+-- GROUP BY
+--     p.name,
+--     a.name,
+--     avg_rating,
+--     p.price,
+--     a.price,
+--     p.genres,
+--     a.content_rating,
+--     a.rating,
+--     p.rating
+-- --HAVING ROUND((a.rating + p.rating) / 2, 2) >= 4
+-- ORDER BY avg_rating DESC)
+-- SELECT appstore_purchase_price, playstore_purchase_price, final_purchase_price, a.name, avg_rating, life_expectancy,
+-- 	(final_purchase_price * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_revenue,
+-- 	(final_purchase_price * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_cost,
 	
+-- 	(final_purchase_price * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)
+-- 	- (final_purchase_price * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12):: MONEY AS expected_profit
+-- FROM app_trader_table
+-- INNER JOIN app_store_apps AS a
+-- ON app_trader_table.apple_store_apps = a.name
+-- INNER JOIN play_store_apps AS p
+-- ON app_trader_table.play_store_apps = p.name;
+-- --george calculations
+-- 	--(10000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_revenue,
+-- 	--((1000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_cost,
+-- 	--(((10000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)
+-- 	-- 	- (1000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)) :: MONEY AS expected_profit
+-- 	WITH c1 AS (
+-- SELECT
+-- 	a.name, 	
+-- 	ROUND((a.rating + p.rating), 0)/2 AS avg_rating,
+-- 	(((ROUND((a.rating + p.rating),0)/2) * 0.2) + 0.1) * 10 AS life_expectancy
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name),
+-- c2 AS (
+-- SELECT 	
+-- 	a.name,
+-- 	CASE
+-- 		WHEN CAST(p.price AS MONEY) > CAST('0' AS MONEY) THEN CAST(p.price AS MONEY) * 10000
+-- 		WHEN CAST (p.price AS MONEY)= CAST('0' AS MONEY) THEN ('10000')
+-- 		END AS playstore_purchase_price,
+-- 	CASE
+-- 		WHEN a.price > 0 THEN a.price * 10000 :: MONEY
+-- 		WHEN a.price = 0 THEN ('10000') :: MONEY
+-- 		END AS appstore_purchase_price,
+-- 	CASE
+-- 		WHEN CAST(p.price AS MONEY) > a.price::MONEY THEN p.price
+-- 		WHEN CAST(p.price AS MONEY) < a.price::MONEY THEN '19900.00'
+-- 		WHEN CAST (p.price AS MONEY) = a.price::MONEY THEN p.price
+-- 		END AS final_purchase_price
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name),
+-- c3 AS (
+-- 	SELECT
+-- 	a.name,
+--   	(10000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_revenue,
+--    	((1000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_cost,
+--    	(((10000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)
+--    	 	- (1000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)) :: MONEY AS expected_profit
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name)
+-- SELECT
+-- 	p.name,
+-- 	a.name,
+-- 	c1.avg_rating,
+-- 	c1.life_expectancy,
+-- 	c2.playstore_purchase_price,
+-- 	c2.appstore_purchase_price,
+-- 	c2.final_purchase_price,
+-- 		c3.expected_revenue,
+--  		c3.expected_cost,
+-- 	 	c3.expected_profit
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name
+-- INNER JOIN c1
+-- ON a.name = c1.name
+-- INNER JOIN c2
+-- ON a.name = c2.name
+-- INNER JOIN c3
+-- ON a.name = C3.name
+-- GROUP BY
+-- 	p.name,
+-- 	a.name,
+-- 	a.rating,
+-- 	p.rating,
+-- 	c1.avg_rating,
+-- 	c1.life_expectancy,
+-- 	c2.playstore_purchase_price,
+-- 	c2.appstore_purchase_price,
+-- 	c2.final_purchase_price,
+-- 	c3.expected_revenue,
+-- 	c3.expected_cost,
+-- 	c3.expected_profit
+-- HAVING ROUND((a.rating + p.rating) / 2, 2) >= 4
+-- --ORDER BY avg_rating DESC
+-- ORDER BY expected_profit DESC
+-- LIMIT 10;
+
+
+
+
+
+
+
+
+
+-- WITH c1 AS (
+-- SELECT
+-- 	a.name, 	
+-- 	ROUND((a.rating + p.rating), 0)/2 AS avg_rating,
+-- 	(((ROUND((a.rating + p.rating),0)/2) * 0.2) + 0.1) * 10 AS life_expectancy
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name),
+-- c2 AS (
+-- SELECT 	
+-- 	a.name,
+-- 	CASE
+-- 		WHEN CAST(p.price AS MONEY) > CAST('0' AS MONEY) THEN CAST(p.price AS MONEY) * 10000
+-- 		WHEN CAST (p.price AS MONEY)= CAST('0' AS MONEY) THEN ('10000')
+-- 		END AS playstore_purchase_price,
+-- 	CASE
+-- 		WHEN a.price > 0 THEN a.price * 10000 :: MONEY
+-- 		WHEN a.price = 0 THEN ('10000') :: MONEY
+-- 		END AS appstore_purchase_price,
+-- 	CASE
+-- 		WHEN CAST(p.price AS MONEY) > a.price::MONEY THEN p.price
+-- 		WHEN CAST(p.price AS MONEY) < a.price::MONEY THEN '19900.00'
+-- 		WHEN CAST (p.price AS MONEY) = a.price::MONEY THEN '10000'
+-- 		END AS final_purchase_price,
+-- 	COALESCE(GREATEST(CAST(CAST(p.price AS money)*10000 AS NUMERIC),a.price * 10000),'10000') :: MONEY
+--         AS scnd_final_purchase_price
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name),
+-- c3 AS (
+-- 	SELECT
+-- 	a.name,
+--   	(10000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_revenue,
+--    	((1000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12) :: MONEY AS expected_cost,
+--    	(((10000 * (((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)
+--    	 	- (1000 * ((((ROUND((a.rating + p.rating) / 2, 2)) * 0.2) + 0.1) * 10) * 12)) :: MONEY AS expected_profit
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name)
+-- SELECT
+-- 	p.name,
+-- 	a.name,
+-- 	c1.avg_rating,
+-- 	c1.life_expectancy,
+-- 	c2.playstore_purchase_price,
+-- 	c2.appstore_purchase_price,
+-- 	c2.final_purchase_price,
+-- 	c2.scnd_final_purchase_price,
+-- 		c3.expected_revenue,
+--  		c3.expected_cost,
+-- 	 	c3.expected_profit
+-- FROM play_store_apps AS p
+-- INNER JOIN app_store_apps AS a
+-- ON p.name=a.name
+-- INNER JOIN c1
+-- ON a.name = c1.name
+-- INNER JOIN c2
+-- ON a.name = c2.name
+-- INNER JOIN c3
+-- ON a.name = C3.name
+-- GROUP BY
+-- 	p.name,
+-- 	a.name,
+-- 	a.rating,
+-- 	p.rating,
+-- 	c1.avg_rating,
+-- 	c1.life_expectancy,
+-- 	c2.playstore_purchase_price,
+-- 	c2.appstore_purchase_price,
+-- 	c2.final_purchase_price,
+-- 	c2.scnd_final_purchase_price,
+-- 	c3.expected_revenue,
+-- 	c3.expected_cost,
+-- 	c3.expected_profit
+-- HAVING ROUND((a.rating + p.rating) / 2, 2) >= 4
+-- --ORDER BY avg_rating DESC
+-- ORDER BY expected_profit DESC
+-- LIMIT 10;
+-- ---Works Needs to be Cleaned Up
